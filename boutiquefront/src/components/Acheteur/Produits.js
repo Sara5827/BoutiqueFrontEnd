9b26e62777ee0d58b78460a8dfbbd54384f2c525
ouldjelloulsara@gmail.com <ouldjelloulsara@gmail.com>
2021-03-01 12:28:54 +0100

    create show a product page

--- boutiquefront/src/components/Acheteur/Produits.js
+++ boutiquefront/src/components/Acheteur/Produits.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function Produits() {
     return (
-        <div>
+        <div className="d-flex">
             <div class="card">
                     <a href="/acheteur/produit/detail">
                         <div  class="card-image"><img width="400px" src="https://damienflandrin.fr/storage/tutoriel-realiser-une-carte-en-css/card-image.jpg" alt="Orange" /></div>
@@ -20,6 +20,40 @@ function Produits() {
                         </div>        
                     </a>
                 </div>
+                <div class="card">
+                    <a href="/acheteur/produit/detail">
+                        <div  class="card-image"><img width="400px" src="https://damienflandrin.fr/storage/tutoriel-realiser-une-carte-en-css/card-image.jpg" alt="Orange" /></div>
+                        <div class="card-body">      
+                            <div class="card-date">
+                                <time>20 Novembre 1992</time>
+                            </div> 
+                            <div class="card-title">
+                                <h3>Lorem Ipsum</h3>
+                            </div> 
+                            <div class="card-excerpt">
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam voluptatibus autem consectetur voluptate facere at, omnis ab optio placeat officiis! Animi modi harum enim quia veniam consectetur unde autem inventore.</p>
+                            </div>
+                        </div>        
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="/acheteur/produit/detail">
+                        <div  class="card-image"><img width="400px" src="https://damienflandrin.fr/storage/tutoriel-realiser-une-carte-en-css/card-image.jpg" alt="Orange" /></div>
+                        <div class="card-body">      
+                            <div class="card-date">
+                                <time>20 Novembre 1992</time>
+                            </div> 
+                            <div class="card-title">
+                                <h3>Lorem Ipsum</h3>
+                            </div> 
+                            <div class="card-excerpt">
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam voluptatibus autem consectetur voluptate facere at, omnis ab optio placeat officiis! Animi modi harum enim quia veniam consectetur unde autem inventore.</p>
+                            </div>
+                        </div>        
+                    </a>
+                </div>
+
+
 
         </div>
     )
